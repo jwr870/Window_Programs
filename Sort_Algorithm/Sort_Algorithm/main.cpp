91cd5ab0f2cb7c55009bbdaff2ef1a0885446962
jwr870 <67399842+jwr870@users.noreply.github.com>
2021-07-12 23:09:33 +0900

    main 모듈 추가

---
+++ Sort_Algorithm/Sort_Algorithm/main.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <limits.h>
+
+#define SIZE 1000
+
+int a[SIZE];
+
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main(void)
+{
+	int n, min, index;
+	scanf("%d", &n);
+
+	return 0;
+}
